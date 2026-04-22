ec9adaab7f2ea02155b668d5096c6aaf1ce05079
fenny09-9 <patelfenny8@gmail.com>
2026-04-22 18:38:05 +0530

    Add files via upload

---
+++ data types.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main(){
+	char ch = 'm'; //char => 1 byte %c
+	int x=100;//int => 4 byte %d
+	float pi=3.14; //float => 4 byte %f
+	printf("\n char =%c ,ch");
+	printf("\n x =%d ,int");
+	printf("\n pi =%f ,float");
+}
