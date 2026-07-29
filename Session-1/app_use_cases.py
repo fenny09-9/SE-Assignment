d80c76e676e486f0ff5f12d27510cf6354208618
unknown <patelfenny8@gmail.com>
2026-07-29 16:21:10 +0530

    Add Session-1

---
+++ Session-1/app_use_cases.py
@@ -0,0 +1,8 @@
+print("1. Instagram")
+print("Python can be used for image processing and recommendation algorithms.")
+
+print("\n 2. WhatsApp")
+print("Python can be used for backend services and data analysis.")
+
+print("\n 3. YouTube")
+print("Python can be used to recommend videos and analyze user data.")
