0a1f4fb2631ead7ff08630f93dbe2f95ac7a580e
unknown <patelfenny8@gmail.com>
2026-08-03 18:29:20 +0530

    Add Session-11

---
+++ Session-11/dic.py
@@ -0,0 +1,7 @@
+my_playlist = {
+    "Perfect": 4,
+    "Believer": 3,
+    "Starboy": 4
+}
+
+print("My Playlist:-", my_playlist)
---
+++ Session-11/display-friends.py
@@ -0,0 +1,8 @@
+friends = {
+    "fenny": 2.3,
+    "riya": 5.6,
+    "tanvi": 3.1
+}
+
+for username, followers in friends.items():
+    print(username + ":", str(followers) + "K followers")
