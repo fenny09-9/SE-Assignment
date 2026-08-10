73b0543d38a8025363fc38eb038ba639ce0cfbaa
unknown <patelfenny8@gmail.com>
2026-08-10 18:58:53 +0530

    Add Python mod 2 Session 11

---
+++ Python-mod-2/Session-11/shuffle_playlist.py
@@ -0,0 +1,16 @@
+import random
+
+songs = [
+    "Starboy",
+    "Trust Issues",
+    "Freaks",
+    "One Of The Girls",
+    "The Hills",
+    "I Wanna Be Yours",
+    "After Dark",
+    "Sweater Weather"
+]
+
+playlist = random.sample(songs, 3)
+
+print("Today's Playlist:", playlist)
